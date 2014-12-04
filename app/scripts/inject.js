54980288a0fb79650c666b477308a4bcb1232641
Lukasz Lipinski <uzza17@gmail.com>
2014-12-04 22:47:56 +0100

    * Enlarging photos in 'Partner for you' section

--- app/scripts/inject.js
+++ app/scripts/inject.js
@@ -1,4 +1,40 @@
-$(document).on('click', '.btn_gods_spells', function() {
+var $body = $('body');
+
+var features = {
+	initialize : function() {
+		this.cleanUpNotNecessaryStuff();
+		this.zooming();
+	},
+
+	cleanUpNotNecessaryStuff : function() {
+		$body.find('#fn_viewQuestions').remove();
+		$body.find('#callKismetlHelp').remove();
+		$body.find('#kismet-unsend .grey').remove();
+	},
+
+	zooming : function() {
+		var html =
+			'<div class="ed_pro_tools">' +
+			'<div class="btn_enlarge_photo ed_pro_button enlarge" title="Open full image in new tab"></div>' +
+			'<div>';
+
+		$body.find('#kistmetBigCard-photoWrapp').append(html);
+
+		$body.on('click', '.btn_enlarge_photo', function() {
+			var url = $("#photoMG, #bigImage").css("backgroundImage").replace("/0/3", "/0/0");
+
+			window.open(url.replace("/0/3", "/0/0").substr(4, url.length - 5));
+		});
+	}
+};
+
+features.initialize();
+
+
+
+
+
+/*$(document).on('click', '.btn_gods_spells', function() {
 	var title = document.title;
 	var url = window.location.href;
 
@@ -9,4 +45,6 @@ $(document).on('click', '.btn_gods_spells', function() {
 			url: url
 		}
 	});
-});
+});*/
+
+//
