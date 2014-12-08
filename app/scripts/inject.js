5b96f77fab7394c3fbe795ec85e06677be3f8789
Lukasz Lipinski <uzza17@gmail.com>
2014-12-08 21:04:17 +0100

    * Photo enlarging in Profile gallery

--- app/scripts/inject.js
+++ app/scripts/inject.js
@@ -3,7 +3,9 @@ var $body = $('body');
 var features = {
 	initialize : function() {
 		this.cleanUpNotNecessaryStuff();
-		this.zooming();
+
+		this.initializePartnerForYouEnlarger();
+		this.initializeProfileEnlarger();
 	},
 
 	cleanUpNotNecessaryStuff : function() {
@@ -12,16 +14,48 @@ var features = {
 		$body.find('#kismet-unsend .grey').remove();
 	},
 
-	zooming : function() {
+	initializePartnerForYouEnlarger : function() {
+		var $button_container = $body.find('#kistmetBigCard-photoWrapp'),
+			$photo = $("#photoMG");
+
+		this.zooming1($button_container, $photo);
+	},
+
+	zooming1 : function($button_container, $photo) {
 		var html =
-			'<div class="ed_pro_tools">' +
-			'<div class="btn_enlarge_photo ed_pro_button enlarge" title="Open full image in new tab"></div>' +
+			'<div class="ed_pro_tools ed_pro_tools1">' +
+				'<div class="btn_enlarge_photo ed_pro_button enlarge" title="Open full image in new tab"></div>' +
 			'<div>';
 
-		$body.find('#kistmetBigCard-photoWrapp').append(html);
+		$button_container.append(html);
 
 		$body.on('click', '.btn_enlarge_photo', function() {
-			var url = $("#photoMG, #bigImage").css("backgroundImage").replace("/0/3", "/0/0");
+			var url = $photo.css("backgroundImage");
+
+			window.open(url.replace("/0/3", "/0/0").substr(4, url.length - 5));
+		});
+	},
+
+	initializeProfileEnlarger : function() {
+		var $hook = $body.find("#galleryPopup");
+
+		if ($hook.length === 0) {
+			return;
+		}
+
+		this.zooming2($hook);
+	},
+
+	zooming2 : function($button_container) {
+		var html =
+			'<div class="ed_pro_tools ed_pro_tools2">' +
+				'<div class="btn_enlarge_photo2 ed_pro_button enlarge" title="Open full image in new tab"></div>' +
+			'<div>';
+
+		$button_container.append(html);
+
+		$body.on('click', '.btn_enlarge_photo2', function() {
+			var url = $('.photo.active .img').css("backgroundImage");
 
 			window.open(url.replace("/0/3", "/0/0").substr(4, url.length - 5));
 		});
